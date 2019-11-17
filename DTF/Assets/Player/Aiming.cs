3c03392c0ed627ece842c51a8d776d115c5c6050
Abbie Calandra <acalandr@mi-a6.21-school.ru>
2019-11-17 21:12:50 +0300

    telecines

--- DTF/Assets/Player/Aiming.cs
+++ DTF/Assets/Player/Aiming.cs
@@ -21,6 +21,8 @@ public class Aiming : MonoBehaviour
         status_wait = true;
         status_aiming = false;
         status_fire = false;
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        EyeRay.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0);
     }
 
     // Update is called once per frame
@@ -53,11 +55,14 @@ public class Aiming : MonoBehaviour
         }
         if (status_fire)
         {
-            this.transform.rotation = Quaternion.FromToRotation(Vector3.right, player.eyes_position - this.transform.position);
-            Vector3 scale = new Vector3(Vector3.Distance(this.transform.position, player.eyes_position) / this.transform.localScale.x, EyeRay.localScale.y, 1);
-            EyeRay.localScale = scale;
             fire_timer += Time.deltaTime;
-            EyeRay.GetComponent<SpriteRenderer>().color = Color.Lerp(RayColor, new Color(1.0f, 1.0f, 1.0f, 0), (1 / FireDelay) * fire_timer);
+            if (!player.is_enable2tele)
+            {
+                this.transform.rotation = Quaternion.FromToRotation(Vector3.right, player.eyes_position - this.transform.position);
+                Vector3 scale = new Vector3(Vector3.Distance(this.transform.position, player.eyes_position) / this.transform.localScale.x, EyeRay.localScale.y, 1);
+                EyeRay.localScale = scale;
+                EyeRay.GetComponent<SpriteRenderer>().color = Color.Lerp(RayColor, new Color(1.0f, 1.0f, 1.0f, 0), (1 / FireDelay) * fire_timer);
+            }
             if (fire_timer > FireDelay)
             {
                 fire_timer = 0;
