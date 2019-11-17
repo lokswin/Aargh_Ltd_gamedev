b6298872691c0b21a27f1f79955bfede39420b6e
Abbie Calandra <acalandr@mi-a6.21-school.ru>
2019-11-17 18:20:14 +0300

    aiming

--- DTF/Assets/Player/Contoller.cs
+++ DTF/Assets/Player/Contoller.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Contoller : MonoBehaviour
 {
+    public Transform EyeRay;
+    public Vector2 displace_aiming;
     private bool is_enable2fly;
     private bool is_XY;
+    private bool is_swiming;
+
     private Vector2 displace_X;
     private Vector2 displace_Y;
+
     public float speed = 5;
+
     private Rigidbody2D Player;
     private BoxCollider2D P_Collider;
     private Vector2 ColliderVertical;
@@ -19,14 +25,35 @@ public class Contoller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        is_enable2fly = false;
-        is_XY = false;
         Player = GetComponent<Rigidbody2D>();
         P_Collider = GetComponent<BoxCollider2D>();
         ColliderVertical = P_Collider.size;
         ColliderHorizontal = new Vector2(ColliderVertical.y, ColliderVertical.x);
         Motions = GetComponent<Animator>();
         Sprite = GetComponent<SpriteRenderer>();
+        Reload();
+    }
+
+    public void Reload()
+    {
+        float random = Random.Range(-1.0f, 1.0f);
+
+        if (random > 0)
+        {
+            is_enable2fly = true;
+            is_XY = true;
+            Debug.Log("I can Fly!");
+        }
+        else
+        {
+            is_enable2fly = false;
+            is_XY = false;
+            Debug.Log("I can Swim!");
+        }
+
+        displace_X = new Vector2(1, Random.Range(-0.4f, -0.1f));
+        displace_Y = new Vector2(Random.Range(0.1f, 0.6f), -1);
+        displace_aiming = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
     }
 
     void FixedUpdate()
@@ -36,24 +63,22 @@ public class Contoller : MonoBehaviour
 
         Vector2 velocity = Vector2.zero;
 
-        displace_X = new Vector2(1, -0.2f);
-        displace_Y = new Vector2(0.5f, -1);
-
         Motions.SetFloat("X_Speed", Mathf.Abs(MoveX));
-        if (!is_enable2fly && is_XY)
+
+        if (MoveX < 0) Sprite.flipX = true;
+        else if (MoveX > 0) Sprite.flipX = false;
+
+        if (Mathf.Abs(Player.velocity.y) > 0.0f)
         {
-            Motions.SetBool("is_swiming", true);
             P_Collider.size = ColliderHorizontal;
+            if (is_enable2fly) Motions.SetBool("is_flying", true);
         }
         else
-        { 
-            Motions.SetBool("is_swiming", false);
+        {
             P_Collider.size = ColliderVertical;
+            if (is_enable2fly) Motions.SetBool("is_flying", false);
         }
-        if (MoveX < 0)
-            Sprite.flipX = true;
-        else if (MoveX > 0)
-            Sprite.flipX = false;
+
         if (Mathf.Abs(MoveX) > 0.0f)
         {
             velocity += displace_X * MoveX * speed;
@@ -61,7 +86,7 @@ public class Contoller : MonoBehaviour
         if (Mathf.Abs(MoveY) > 0.0f && is_XY)
         {
             velocity += displace_Y * MoveY * speed;
-        }
+        } 
         Player.velocity = velocity;
     }
 
@@ -69,6 +94,7 @@ public class Contoller : MonoBehaviour
     {
         if (collision.gameObject.tag == "Water")
         {
+            Motions.SetBool("is_swiming", true);
             is_XY = true;
         }
     }
@@ -76,8 +102,18 @@ public class Contoller : MonoBehaviour
     {
         if (collision.gameObject.tag == "Water")
         {
-            is_XY = false;
+            Motions.SetBool("is_swiming", false);
+            if (!is_enable2fly)
+                is_XY = false;
         }
+    }
+
+    public void Fire(Vector3 aim)
+    {
+        Debug.Log("Fire!");
+        EyeRay.rotation = Quaternion.FromToRotation(Vector3.right, this.transform.position - aim);
+        Vector3 scale = new Vector3(Vector3.Distance(this.transform.position, aim) / this.transform.localScale.x, EyeRay.localScale.y, 1);
+        EyeRay.localScale = scale;
     }
 
 }
